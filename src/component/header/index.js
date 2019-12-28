2e9059a301a4eca8e8cd01aef141bb4fa2f98000
Victor Ifeanyi <vifeanyi33@gmail.com>
2019-12-28 19:39:54 +0100

    Header Component Test

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -4,10 +4,10 @@ import Logo from './../../assets/graphics/enzyme.png';
 
 const Header = (props) => {
     return (
-        <header>
+        <header data-test="headerComponent">
             <div className="wrap">
                 <div className="logo">
-                    <img src={Logo} alt="logo" />
+                    <img data-test="logoIMG" src={Logo} alt="logo" />
                 </div>
             </div>
         </header>
