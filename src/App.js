c21ea71b18093d16ccd7ac7922465a086690d084
Victor Ifeanyi <vifeanyi33@gmail.com>
2019-12-28 11:30:01 +0100

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import Header from './component/header';
+import './app.scss';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Header />
     </div>
   );
 }
